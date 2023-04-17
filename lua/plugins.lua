72f6dbff12704413143b46df99416b7e68f5efd3
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-04-17 13:24:22 -0300

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,25 +1,32 @@
+-- Load Packer
 vim.cmd [[packadd packer.nvim]]
 
-require('options')
+-- Rerun PackerCompile everytime pluggins.lua is updated
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
 
+-- Initialize Pluggins
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  -- Start Screen
   use {
     'goolord/alpha-nvim',
     requires = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require'alpha'.setup(require'alpha.themes.startify'.config)
-    end
+    config = function() require'alpha'.setup(require'alpha.themes.startify'.config) end
   }
 
-  -- LuaLine
+  -- Status Line
   use {
     'nvim-lualine/lualine.nvim',
     config = function() require('plugins.lualine') end
   }
 
-  -- NvimTree
+  -- File Explorer
   use {
     'nvim-tree/nvim-tree.lua',
     requires = 'nvim-tree/nvim-web-devicons',
