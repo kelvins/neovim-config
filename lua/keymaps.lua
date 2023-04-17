82df122a98ab9956a55679cfd1d75acdd34ad567
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-04-17 13:35:05 -0300

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,3 +6,9 @@ local nnoremap = Utils.nnoremap
 
 -- File Explorer
 nnoremap('<C-o>', '<Cmd>NvimTreeToggle<CR>')
+
+-- Switching Windows
+nnoremap('<C-j>', '<C-w>j')
+nnoremap('<C-k>', '<C-w>k')
+nnoremap('<C-l>', '<C-w>l')
+nnoremap('<C-h>', '<C-w>h')
