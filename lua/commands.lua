ba063f6a5f9c6ee9440a262e04dd1caddfb3c5bd
Kelvin S. do Prado <kelvinpfw@gmail.com>
2023-05-22 01:10:28 -0300

    Add command to remove trailing whitespaces

--- lua/commands.lua
+++ lua/commands.lua
@@ -15,3 +15,9 @@ vim.api.nvim_create_autocmd('BufEnter', {
     command = "if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif",
     nested = true,
 })
+
+-- Auto remove trailing white spaces
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
